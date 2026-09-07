03f245f63bdf91e116471ac6ceb0903d3bf4b811
MrSaturnIdk <abrahm3314@gmail.com>
2026-09-07 21:21:35 +0000

    refactor: use macros for ansi flags
    Saves me alot of \033

---
+++ include/ansicolors.h
@@ -0,0 +1,11 @@
+/// So I have clear names and not a wall of \033
+
+#ifndef ANSICOLORS_H
+#define ANSICOLORS_H
+
+#define ANSI_RESET "\033[0m"
+#define ANSI_BOLD "\033[1m"
+
+#define ANSI_RED "\033[31m"
+
+#endif
